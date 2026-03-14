229935ae95cc8561dd37c4436a71477b3d50e2e9
Oliverashraf <oliverashraf4@gmail.com>
2026-03-14 02:57:02 +0200

    Initial commit: project setup

---
+++ src/main/java/s52_1257/t_10/oliver_ashraf/OliverAshrafApplication.java
@@ -0,0 +1,13 @@
+package s52_1257.t_10.oliver_ashraf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliverAshrafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OliverAshrafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s52_1257/t_10/oliver_ashraf/OliverAshrafApplicationTests.java
@@ -0,0 +1,13 @@
+package s52_1257.t_10.oliver_ashraf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliverAshrafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
